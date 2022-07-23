9beaf4f9ecef00f65c35068166bb11b3f0e0b008
Abraham <portales.ab@gmail.com>
2022-07-23 17:04:00 -0500

    se agrega dist para pages

--- src/services/components/HomePage/Carousel.jsx
+++ src/services/components/HomePage/Carousel.jsx
@@ -5,22 +5,22 @@ export const Carousel = () => {
             <div id="carouselControls" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel00.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel00"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel00.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel00"/>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel01.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel01"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel01.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel01"/>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel02.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel02"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel02.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel02"/>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel03.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel03"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel03.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel03"/>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel04.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel04"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel04.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel04"/>
                     </div>
                     <div className="carousel-item" data-bs-interval="3000">
-                        <img src="/assets/HomePage/carousel05.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel05"/>
+                        <img src="https://github.com/abportales/malagon-ortodoncia/tree/main/assets/HomePage/carousel05.jpg" className="d-block w-100 animate__animated animate__fadeIn" alt="carousel05"/>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
